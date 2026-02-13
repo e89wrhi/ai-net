3d296d2ebec52fc2ce1a3959e895f6bd4bc4da40
e89wrhi <39992513+e89wrhi@users.noreply.github.com>
2026-02-14 00:18:09 +0300

    Fixed Tracked Module Enum

--- Src/Modules/User/Users/Enums/TrackedModule.cs
+++ Src/Modules/User/Users/Enums/TrackedModule.cs
@@ -2,9 +2,20 @@
 
 public enum TrackedModule
 {
-    ResumeAnalyzer = 0,
-    MeetingSummarizer = 1,
-    ImageCaptioning = 2,
-    Chatbot = 3,
-    LearningAssistant = 4,
+    AutoComplete = 0,
+    Chat = 1,
+    CodeDebug = 2,
+    CodeGen = 3,
+    ImageCaption = 4,
+    ImageEdit = 5,
+    ImageGen = 6,
+    Learn = 7,
+    Meeting = 8,
+    Payment = 9,
+    Resume = 10,
+    Sentiment = 11,
+    SpeechToText = 12,
+    Summary = 13,
+    TextToSpeech = 14,
+    Translate = 15,
 }
